dd9f5652e690cffd868f1c9dc9cd421225f726b1
Jason Holmes <holmes@squareup.com>
2016-09-21 17:14:05 -0700

    Officially remove subcomponents

--- Cleanse/BaseComponent.swift
+++ Cleanse/BaseComponent.swift
@@ -11,11 +11,9 @@ import Foundation
 public protocol _AnyBaseComponent : Configurable {
 }
 
-
 /// Base protocol for both Components and Subcomponenents
-
 public protocol _BaseComponent : _AnyBaseComponent {
-    /// This is the binding required to construct a new subcomponent
+    /// This is the binding required to construct a new Component. Think of it as somewhat of an initialization value.
     associatedtype Seed = Void
 
     /// This should be set to the root type of object that is created.
