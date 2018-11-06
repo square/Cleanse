230ff4c74f411d9e60e4e33f80cf1be9a659ce02
Sebastian Shanus <sebastians@squareup.com>
2018-11-06 14:11:51 -0800

    Remove Singleton Scope.
    This can be renamed down the road.

--- Cleanse/Scope.swift
+++ Cleanse/Scope.swift
@@ -18,7 +18,6 @@ public protocol _ScopeBase {
 public protocol Scope : _ScopeBase {
 }
 
-public struct Singleton : Scope {}
 
 /// This a special scope that means its not scoped
 public struct Unscoped : _ScopeBase {
