be6370427a77dcc9234dc17fcbaa5cfe4469114c
Lite Ye <liteye859@gmail.com>
2019-11-02 21:50:57 -0400

    Fix the Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,8 +1,15 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Cleanse",
+    products: [
+        .library(name: "Cleanse", targets: ["Cleanse"]),
+    ],
+    dependencies: [],
     targets: [
+        .target(name: "Cleanse", dependencies: [], path: "Cleanse"),
     ]
 )
 
