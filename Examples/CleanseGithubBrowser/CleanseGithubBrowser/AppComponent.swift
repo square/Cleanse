6ef9697dfbe72e1e0e50b3536a2773a154c44811
Sebastian Shanus <sebastians@squareup.com>
2020-06-16 13:41:51 -0700

    wip

--- Examples/CleanseGithubBrowser/CleanseGithubBrowser/AppComponent.swift
+++ Examples/CleanseGithubBrowser/CleanseGithubBrowser/AppComponent.swift
@@ -7,6 +7,105 @@
 //
 
 import Cleanse
+import UIKit
+
+// ----------------------- CxFImplWiring
+
+struct CheckoutFlowModule: Module {
+    static func configure(binder: Binder<CheckoutFlowScope>) {
+        binder.bind(Int.self).to(value: 3)
+    }
+    
+    
+}
+
+struct CheckoutFlowModule2: Module {
+    static func configure(binder: Binder<CheckoutFlowScope>) {
+        binder.bind(Int.self).to(value: 5)
+    }
+    
+    
+}
+
+struct CheckoutFlowScope: Scope {}
+
+protocol CheckoutFlowModules: Cleanse.Module where Scope == CheckoutFlowScope {}
+
+struct CheckoutFlowComponent<M:CheckoutFlowModules>: Cleanse.Component {
+    static func configure(binder: Binder<CheckoutFlowScope>) {
+        binder.include(module: M.self)
+    }
+    
+    static func configureRoot(binder bind: ReceiptBinder<String>) -> BindingReceipt<String> {
+        return bind.to(value: "Hello")
+    }
+    
+    typealias Root = String
+    
+    
+}
+
+
+// ----------------------- LoggedInImplWiring
+
+struct LoggedInScope: Scope {}
+
+protocol LoggedInModules: Cleanse.Module where Scope == LoggedInScope {}
+
+struct LoggedInComponent<M:LoggedInModules>: Cleanse.RootComponent {
+    static func configure(binder: Binder<LoggedInScope>) {
+        binder.include(module: M.self)
+    }
+    
+    static func configureRoot(binder bind: ReceiptBinder<UIViewController>) -> BindingReceipt<UIViewController> {
+        return bind.to(value: UIViewController(nibName: nil, bundle: nil))
+    }
+    
+    typealias Root = UIViewController
+    
+    
+}
+
+// ------------------------ App
+
+struct SomeLoggedInModule: Cleanse.Module {
+    static func configure(binder: Binder<LoggedInScope>) {
+        
+    }
+    
+    
+}
+
+struct POSCheckoutFlowModules: CheckoutFlowModules {
+    static func configure(binder: Binder<CheckoutFlowScope>) {
+        binder.include(module: CheckoutFlowModule.self)
+    }
+
+}
+
+struct APOSCheckoutFlowModules: CheckoutFlowModules {
+    static func configure(binder: Binder<CheckoutFlowScope>) {
+        binder.include(module: CheckoutFlowModule.self)
+    }
+
+}
+
+struct POSLoggedInModules: LoggedInModules {
+    static func configure(binder: Binder<LoggedInScope>) {
+        binder.include(module: SomeLoggedInModule.self)
+        
+        binder.install(dependency: CheckoutFlowComponent<POSCheckoutFlowModules>.self)
+    }
+    
+    
+}
+
+struct AppointmentsLoggedInModules: LoggedInModules {
+    static func configure(binder: Binder<LoggedInScope>) {
+        
+    }
+    
+}
 
 struct AppComponent<ServiceModule: GithubServicesModule> : Cleanse.RootComponent where ServiceModule.Scope == Singleton {
     typealias Root = PropertyInjector<AppDelegate>
@@ -15,6 +114,7 @@ struct AppComponent<ServiceModule: GithubServicesModule> : Cleanse.RootComponent
     static func configure(binder: SingletonBinder) {
         binder.include(module: CoreAppModule.self)
         binder.include(module: ServiceModule.self)
+        binder.install(dependency: LoggedInComponent<POSLoggedInModules>.self)
 
         #if DEBUG
         binder.include(module: FakeModeSettingsModule.self)
