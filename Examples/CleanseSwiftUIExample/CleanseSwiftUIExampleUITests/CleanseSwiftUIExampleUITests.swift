9bfe82cf813bb9fbd0f7a50045b5c6464b1617af
chathil <chathil98@gmail.com>
2021-01-02 23:42:47 +0700

    remove ui tests

--- Examples/CleanseSwiftUIExample/CleanseSwiftUIExampleUITests/CleanseSwiftUIExampleUITests.swift
+++ Examples/CleanseSwiftUIExample/CleanseSwiftUIExampleUITests/CleanseSwiftUIExampleUITests.swift
@@ -12,22 +12,7 @@ class CleanseSwiftUIExampleUITests: XCTestCase {
 
     var app: XCUIApplication!
 
-    override func setUp() {
-        super.setUp()
-        app = XCUIApplication()
-        continueAfterFailure = false
-        app.launch()
-    }
-    
     func testExample() throws {
-        let elementsQuery = app.scrollViews.otherElements
-        XCTAssertTrue(elementsQuery.staticTexts["Find"].exists)
-        elementsQuery.buttons["Doe, Some text that supposed\nto describes something, Account"].tap()
-        XCTAssertTrue(elementsQuery.staticTexts["Credits"].exists)
-        app.navigationBars["User Detail"].buttons["Back"].tap()
-        elementsQuery.buttons["Crawler, Bireuen, Aceh, Cat"].tap()
-        XCTAssertTrue(elementsQuery.images["heart.fill"].exists)
-        elementsQuery.images["heart.fill"].tap()
-        XCTAssertTrue(elementsQuery.images["heart"].exists)
+        
     }
 }
